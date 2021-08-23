1c43031cd005700f06c406e77083414adc6d71b4
Carlos Montesinos <carlosmontesinos1990@gmail.com>
2021-08-23 16:23:40 +0200

    not finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  def new
+    # Generar el array de diez letras.
+    @letters = []
+    10.times do
+      @letters << ('a'..'z').to_a.sample
+    end
+
+    # Recoger input del usuario.
+
+    # puts 'What is the longest word you can find?'
+    # @answer = gets.chomp
+  end
+
+  # Iterar sobre las letras de la palabra elegida por le usuario. Si todas .include? @letters el usuario gana.
+
+  def score
+    @win = false
+    @answer.split('').each do |letter|
+      if @letters.include?(letter)
+        @win = true
+      else
+        @win
+      end
+    end
+  end
 end
